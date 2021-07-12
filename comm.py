dc9ddd1bea1a97e3b78bc733aefba5265a0cdcc6
Stephen Gruzin <stephengrzn@gmail.com>
2021-07-12 15:56:43 -0700

    Tweaked the timings

--- comm.py
+++ comm.py
@@ -1,14 +1,11 @@
 import serial
 import time
 
-#ser = serial.Serial('/dev/ttyACM2', 9600, timeout=1)
-ser = serial.Serial('COM5', 9600, timeout=1)
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+#ser = serial.Serial('COM5', 9600, timeout=1)
 ser.flush()
 
 def send_comm(comm):
-        what = comm
-        ser.write(what.encode())
-        time.sleep(1)
-        line = ser.readline().decode('utf-8').rstrip()
-        print(line)
-        time.sleep(1)
+	ser.write(comm.encode())
+	line = ser.readline().decode('utf-8').rstrip()
+	print(line)
