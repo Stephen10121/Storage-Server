2aed0db10f220d28d36ef859ca48a3ae69a9e9d9
Stephen Gruzin <56006255+Stephen10121@users.noreply.github.com>
2021-07-28 15:09:38 -0700

    Update welcome.js

--- static/script/welcome.js
+++ static/script/welcome.js
@@ -3,6 +3,7 @@ let width = box.offsetWidth;
 console.log(width);
 console.log(screen.width);
 console.log(screen.width-width);
+document.getElementById("main-class").style.width = screen.width-width;
 function ShowNotification(what) {
     const notification = new Notification("From stephen15.tk.", {
         body: what
